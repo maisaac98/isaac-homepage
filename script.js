bd122ea5060070d44bf52fc25a8905cce5310632
maisaac98 <maisaac98@gmail.com>
2022-06-12 08:23:38 +1000

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const time = document.getElementById("current-time");
 const welcomeMessage = document.getElementById("welcome-message");
+const apiKey = 'd6d161bb9d1040aa8cd75119221106';
+const weatherMessage = document.getElementById('weather');
+const weatherIcon = document.getElementById('weather-icon');
+//read only API key
 
+let currentWeather = '';
 let updatedTime = '';
 
 setInterval(() =>{
@@ -12,13 +17,27 @@ setInterval(() =>{
         welcomeMessage.textContent = "Good Evening Isaac!"
     }
     else if(currentTime.getHours() > 11){
-        welcomeMessage.textContent("Good Afternoon Isaac!");
+        welcomeMessage.textContent = "Good Afternoon Isaac!";
     }
     else{
-        welcomeMessage.textContent("Good Morning Isaac!");
+        welcomeMessage.textContent = "Good Morning Isaac!";
     }
 }, 1000);
 
-//add weather api, then add https://codepen.io/mgkOV/pen/qzNegg
-//weather effect depending on cold, warm, or rain
-//add to github pages
+async function getWeather(){
+    try{
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=Sydney`);
+        currentWeather = await response.json();
+        console.log(currentWeather);
+        console.log(currentWeather.current.condition.text);
+        weatherMessage.textContent = currentWeather.current.condition.text + ' ' + currentWeather.current.temp_c + 'C';
+        
+        weatherIcon.setAttribute('src', `https:${currentWeather.current.condition.icon}`);
+        weatherMessage.appendChild(weatherIcon);
+    }
+    catch{
+        console.log(error);
+    }
+}
+
+getWeather();
